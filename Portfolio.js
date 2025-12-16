c917003c5f24821257433313064b6b3b2e149d5e
ShoheiK12 <shohei.kotera22@gmail.com>
2025-12-16 17:40:49 +1100

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,6 @@
+/* 最初にアニメーションが表示される（そのほかは背景色(黒色)で隠されている）。そのあと、Javascriptのwindow.onloadが発火し、3秒待った後に、bodyにloadedクラスが追加され、CSSのbody.loaded .animationのCSSが動く、つまり、アニメーションのopacityは0(フェードアウト)になり、アニメーションはhiddenされる。その後、CSSのtransition: opacity 0.8s ease, visibility 0.8s ease;により、0.8 秒かけて自然に消える。 */
+window.onload = function () {
+    setTimeout(function () {
+      document.body.classList.add('loaded');
+    }, 3000); 
+  };
