b39af50a4fdd0718eb338411b0af46fc07208d78
Shohei <shohei.kotera22@gmail.com>
2026-03-14 17:27:41 +1100

    Refactor Portfolio.js for improved readability
    Add commnet to loading animation

--- Portfolio.js
+++ Portfolio.js
@@ -1,4 +1,9 @@
-/* 最初にアニメーションが表示される（そのほかは背景色(黒色)で隠されている）。そのあと、Javascriptのwindow.onloadが発火し、3秒待った後に、bodyにloadedクラスが追加され、CSSのbody.loaded .animationのCSSが動く、つまり、アニメーションのopacityは0(フェードアウト)になり、アニメーションはhiddenされる。その後、CSSのtransition: opacity 0.8s ease, visibility 0.8s ease;により、0.8 秒かけて自然に消える。 */
+/*
+Initially, an animation is displayed while everything else is hidden by a black background. 
+Afterwards, the Javascript window.onload event fires. After a 3-second delay, the loaded class is added to the body element. 
+This triggers the CSS for body.loaded .animation, causing the animation's opacity to become 0 (fading out) and the animation to be hidden. 
+The animation then naturally disappears over 0.8 seconds due to the CSS transition transition: opacity 0.8s ease, visibility 0.8s ease;.
+*/
 window.onload = function () {
     setTimeout(function () {
       document.body.classList.add('loaded');
