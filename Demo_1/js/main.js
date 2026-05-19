24771ba4c0da9cc81928caefcaac835336747363
Shohei <shohei.kotera22@gmail.com>
2026-05-19 17:16:17 +1000

    Add checks and error handling for swiper and products
    Added checks for the existence of swiper and product list elements before initializing them. Added error handling for the product list fetching process.

--- Demo_1/js/main.js
+++ Demo_1/js/main.js
@@ -1,52 +1,54 @@
 document.addEventListener('DOMContentLoaded', async() => {
   /*=================================================
-  Initialise Swiper
+  Initialise Swiper (Execute only when there is swiper)
   ===================================================*/
-  const swiper = new Swiper(".swiper", {
-
-    loop: true,
-
-    speed: 1000,
-
-    autoplay: {
-      delay: 3000,
-    },
-
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-  });
+  const swiperElement = document.querySelector(".swiper");
+  
+  if (swiperElement) {
+    const swiper = new Swiper(".swiper", {
+      loop: true,
+      speed: 1000,
+      autoplay: {
+        delay: 3000,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
 
   
   /*=================================================
   Generate Product List
   ===================================================*/
-  const response = await fetch('./products.json');
-
-  const products = await response.json();
-
   const productList = document.querySelector('.product-list');
 
-  products.forEach(product => {
+  // Implemet only when .product-list exists.
+  if (productList) {
+    try {
+      const response = await fetch('./products.json');
+      const products = await response.json();
 
-    const li = document.createElement('li');
+      products.forEach(product => {
+        const li = document.createElement('li');
 
-    li.innerHTML = `
-      <a href="item.html?id=${product.id}">
-        <img src="${product.image}" alt="">
-        <p>${product.title}</p>
-        <p>${product.price}</p>
-      </a>
-    `;
+        li.innerHTML = `
+          <a href="item.html?id=${product.id}">
+            <img src="${product.image}" alt="">
+            <p>${product.title}</p>
+            <p>${product.price}</p>
+          </a>
+        `;
 
-    productList.appendChild(li);
-  
-  });
+        productList.appendChild(li);
+      });
+    } catch (error) {
+      console.error("Data downloading failed:", error);
+    }
+  }
 });
